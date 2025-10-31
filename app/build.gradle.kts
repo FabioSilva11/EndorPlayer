50ba42857cecba76ee8d475e4489662a89c911d3
FabioSilva11 <produtorfabiosilva@gmail.com>
2025-10-31 09:44:36 -0400

    firebase adicionado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -40,4 +42,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation("com.google.android.exoplayer:exoplayer:2.19.1")
     implementation("com.google.android.material:material:1.12.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx")
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
 }
